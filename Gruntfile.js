9597b47d5d3a9c8a9b3ad8151b8ff3786671ec49
Ami G <bunchopunch@mustacheforpresident.com>
2014-11-04 16:09:31 -0700

    Updating asset paths for build

--- Gruntfile.js
+++ Gruntfile.js
@@ -229,10 +229,10 @@ module.exports = function (grunt) {
       dist: {
         files: {
           src: [
-            '<%= config.dist %>/scripts/{,*/}*.js',
-            '<%= config.dist %>/styles/{,*/}*.css',
-            '<%= config.dist %>/images/{,*/}*.*',
-            '<%= config.dist %>/styles/fonts/{,*/}*.*',
+            '<%= config.dist %>/scripts/**/*.js',
+            '<%= config.dist %>/styles/**/*.css',
+            '<%= config.dist %>/images/**/*.*',
+            '<%= config.dist %>/styles/fonts/**.*',
             '<%= config.dist %>/*.{ico,png}'
           ]
         }
